f391bd444bd4511665401d9bc3be74695333700a
SkippieDippie <skippiedipie@gmail.com>
2022-09-12 22:01:40 +0200

    GreenMag: Started working on PKM/PKP boxes

---
+++ cvo_rhsafrf_compats/greenmag/belts/afrf_762x54/config.cpp
@@ -0,0 +1,18 @@
+#include "\z\cvo_rhsafrf_compats\greenmag\macros.hpp"
+
+class CfgPatches {
+	class CVO_RHSAFRF_Compats_GreenMag_762x54_Belts {
+		addonRootClass = "CVO_RHSAFRF_Compats_GreenMag";
+
+		requiredAddons[] = {"greenmag_main"};
+		requiredVersion = 1.00;
+
+		units[] = {};
+		weapons[] = {
+		};
+	};
+};
+
+class CfgWeapons {
+
+};
--- cvo_rhsafrf_compats/greenmag/magazines/afrf_762x54/config.cpp
+++ cvo_rhsafrf_compats/greenmag/magazines/afrf_762x54/config.cpp
@@ -14,7 +14,7 @@ class CfgMagazines {
 	class rhs_30Rnd_545x39_7N6_AK;
 
 	class rhs_10Rnd_762x54mmR_7N1 : rhs_30Rnd_545x39_7N6_AK {
-		greenmag_canSpeedload = 1;
+		greenmag_canSpeedload = 0;
 		greenmag_needBelt = 0;
 		greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_762x54_7N1_1Rnd";
 		greenmag_basicammo = "greenmag_ammo_762x54_basic_1Rnd";		
@@ -22,5 +22,28 @@ class CfgMagazines {
 
 	class rhs_10Rnd_762x54mmR_7N14 : rhs_10Rnd_762x54mmR_7N1 {
 		greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_762x54_7N14_1Rnd";
+	};
+
+	class rhs_100Rnd_762x54mmR : rhs_30Rnd_545x39_7N6_AK {
+		greenmag_canSpeedload = 0;
+		greenmag_needBelt = 1;
+		greenmag_ammo = "greenmag_beltlinked_762x54_basic";
+		greenmag_basicammo = "greenmag_beltlinked_762x54_basic";
+	};
+
+	class rhs_100Rnd_762x54mmR_7N13 : rhs_100Rnd_762x54mmR {
+		greenmag_ammo = "greenmag_beltlinked_762x54_basic";
+	};
+
+	class rhs_100Rnd_762x54mmR_7N26 : rhs_100Rnd_762x54mmR {
+		greenmag_ammo = "greenmag_beltlinked_762x54_basic";
+	};
+
+	class rhs_100Rnd_762x54mmR_green : rhs_100Rnd_762x54mmR {
+		greenmag_ammo = "greenmag_beltlinked_762x54_basic";
+	};
+
+	class rhs_100Rnd_762x54mmR_7BZ3 : rhs_100Rnd_762x54mmR_7N13 {
+		greenmag_ammo = "greenmag_beltlinked_762x54_basic";
 	};
 };
