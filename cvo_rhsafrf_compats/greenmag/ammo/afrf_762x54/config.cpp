3f006a38bd7f47858dffa8e63e7e83497d39691b
SkippieDippie <skippiedipie@gmail.com>
2022-09-12 20:22:57 +0200

    Added GreenMag compat for 7.62x54 SVD magazines

---
+++ cvo_rhsafrf_compats/greenmag/ammo/afrf_762x54/config.cpp
@@ -0,0 +1,48 @@
+#include "\z\cvo_rhsafrf_compats\greenmag\macros.hpp"
+
+class CfgPatches {
+	class CVO_RHSAFRF_Compats_GreenMag_545x39_Ammo {
+		addonRootClass = "CVO_RHSAFRF_Compats_GreenMag";
+
+		requiredAddons[] = {"greenmag_main"};
+		requiredVersion = 1.00;
+
+		units[] = {};
+		weapons[] = {
+			"CVO_GreenMag_AFRF_Ammo_762x54_7N1_1Rnd",
+			"CVO_GreenMag_AFRF_Ammo_762x54_7N1_30Rnd",
+			"CVO_GreenMag_AFRF_Ammo_762x54_7N1_60Rnd",
+			"CVO_GreenMag_AFRF_Ammo_762x54_7N14_1Rnd",
+			"CVO_GreenMag_AFRF_Ammo_762x54_7N14_30Rnd",
+			"CVO_GreenMag_AFRF_Ammo_762x54_7N14_60Rnd"
+		};
+	};
+};
+
+class CfgWeapons {
+	class greenmag_core_box;
+	class greenmag_core_1Rnd : greenmag_core_box {
+		class ItemInfo;
+	};
+	class greenmag_core_10Rnd : greenmag_core_box {
+		class ItemInfo;
+	};
+	class greenmag_core_20Rnd : greenmag_core_box {
+		class ItemInfo;
+	};
+	class greenmag_core_30Rnd : greenmag_core_box {
+		class ItemInfo;
+	};
+	class greenmag_core_40Rnd : greenmag_core_box {
+		class ItemInfo;
+	};
+	class greenmag_core_50Rnd : greenmag_core_box {
+		class ItemInfo;
+	};
+	class greenmag_core_60Rnd : greenmag_core_box {
+		class ItemInfo;
+	};
+
+	AMMO_CLASS(762x54_7N1,7.62x54 7N1,0.48)
+	AMMO_CLASS(762x54_7N14,7.62x54 7N14,0.48)
+};
---
+++ cvo_rhsafrf_compats/greenmag/magazines/afrf_762x54/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches {
+	class CVO_RHSAFRF_Compats_GreenMag_545x39_Magazines {
+		addonRootClass = "CVO_RHSAFRF_Compats_GreenMag";
+
+		requiredAddons[] = {"rhs_c_weapons"};
+		requiredVersion = 1.00;
+
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgMagazines {
+	class rhs_30Rnd_545x39_7N6_AK;
+
+	class rhs_10Rnd_762x54mmR_7N1 : rhs_30Rnd_545x39_7N6_AK {
+		greenmag_canSpeedload = 1;
+		greenmag_needBelt = 0;
+		greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_762x54_7N1_1Rnd";
+		greenmag_basicammo = "greenmag_ammo_762x54_basic_1Rnd";		
+	};
+
+	class rhs_10Rnd_762x54mmR_7N14 : rhs_10Rnd_762x54mmR_7N1 {
+		greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_762x54_7N14_1Rnd";
+	};
+};
