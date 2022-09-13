5d6429a9fe32cc2e2b6f4dd558edd13359318ca9
SkippieDippie <skippiedipie@gmail.com>
2022-09-13 19:43:55 +0200

    Restructure, cleanup and fix typo in base

--- cvo_rhsafrf_compats/greenmag/ammo/afrf_545x39.hpp
+++ cvo_rhsafrf_compats/greenmag/ammo/afrf_545x39.hpp
@@ -1,3 +1,4 @@
+// 7N6
 class AMMO_CLASS(545x39_7N6_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
 	displayName = "5.45 7N6 - 1Rnd";
 	picture = "\z\greenmag\addons\main\data\556mm.paa";
@@ -30,11 +31,15 @@ class AMMO_CLASS(545x39_7N6_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
+	greenmag_ammotype = AMMO_CLASS(545x39_7N6_1Rnd);
+
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 13.2;
 	};
 };
 
+
+// 7N6M
 class AMMO_CLASS(545x39_7N6M_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
 	displayName = "5.45 7N6M - 1Rnd";
 	picture = "\z\greenmag\addons\main\data\556mm.paa";
@@ -53,7 +58,7 @@ class AMMO_CLASS(545x39_7N6M_30Rnd) : CVO_GreenMag_Ammo_Core_30Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
-	greenmag_ammotype = AMMO_CLASS(545x39_7N6_1Rnd);
+	greenmag_ammotype = AMMO_CLASS(545x39_7N6M_1Rnd);
 
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 6.6;
@@ -67,11 +72,15 @@ class AMMO_CLASS(545x39_7N6M_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
+	greenmag_ammotype = AMMO_CLASS(545x39_7N6M_1Rnd);
+
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 13.2;
 	};
 };
 
+
+// 7N10
 class AMMO_CLASS(545x39_7N10_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
 	displayName = "5.45 7N10 - 1Rnd";
 	picture = "\z\greenmag\addons\main\data\556mm.paa";
@@ -90,7 +99,7 @@ class AMMO_CLASS(545x39_7N10_30Rnd) : CVO_GreenMag_Ammo_Core_30Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
-	greenmag_ammotype = AMMO_CLASS(545x39_7N6_1Rnd);
+	greenmag_ammotype = AMMO_CLASS(545x39_7N10_1Rnd);
 
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 6.6;
@@ -104,11 +113,15 @@ class AMMO_CLASS(545x39_7N10_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
+	greenmag_ammotype = AMMO_CLASS(545x39_7N10_1Rnd);
+
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 13.2;
 	};
 };
 
+
+// 7N22
 class AMMO_CLASS(545x39_7N22_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
 	displayName = "5.45 7N22 - 1Rnd";
 	picture = "\z\greenmag\addons\main\data\556mm.paa";
@@ -127,7 +140,7 @@ class AMMO_CLASS(545x39_7N22_30Rnd) : CVO_GreenMag_Ammo_Core_30Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
-	greenmag_ammotype = AMMO_CLASS(545x39_7N6_1Rnd);
+	greenmag_ammotype = AMMO_CLASS(545x39_7N22_1Rnd);
 
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 6.6;
@@ -141,11 +154,15 @@ class AMMO_CLASS(545x39_7N22_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
+	greenmag_ammotype = AMMO_CLASS(545x39_7N22_1Rnd);
+
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 13.2;
 	};
 };
 
+
+// 7T3M
 class AMMO_CLASS(545x39_7T3M_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
 	displayName = "5.45 7T3M - 1Rnd";
 	picture = "\z\greenmag\addons\main\data\556mm.paa";
@@ -164,7 +181,7 @@ class AMMO_CLASS(545x39_7T3M_30Rnd) : CVO_GreenMag_Ammo_Core_30Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
-	greenmag_ammotype = AMMO_CLASS(545x39_7N6_1Rnd);
+	greenmag_ammotype = AMMO_CLASS(545x39_7T3M_1Rnd);
 
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 6.6;
@@ -178,11 +195,15 @@ class AMMO_CLASS(545x39_7T3M_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
+	greenmag_ammotype = AMMO_CLASS(545x39_7T3M_1Rnd);
+
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 13.2;
 	};
 };
 
+
+// 7U1
 class AMMO_CLASS(545x39_7U1_1Rnd) : CVO_GreenMag_Ammo_Core_1Rnd {
 	displayName = "5.45 7U1 - 1Rnd";
 	picture = "\z\greenmag\addons\main\data\556mm.paa";
@@ -201,7 +222,7 @@ class AMMO_CLASS(545x39_7U1_30Rnd) : CVO_GreenMag_Ammo_Core_30Rnd {
 	scopeArsenal=2;
 	scopeCurator=2;
 
-	greenmag_ammotype = AMMO_CLASS(545x39_7N6_1Rnd);
+	greenmag_ammotype = AMMO_CLASS(545x39_7U1_1Rnd);
 
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 7.2;
@@ -214,6 +235,8 @@ class AMMO_CLASS(545x39_7U1_60Rnd) : CVO_GreenMag_Ammo_Core_60Rnd {
 	scope = 2;
 	scopeArsenal=2;
 	scopeCurator=2;
+
+	greenmag_ammotype = AMMO_CLASS(545x39_7U1_1Rnd);
 
 	class ItemInfo : CBA_MiscItem_ItemInfo {
 		mass = 14.4;
