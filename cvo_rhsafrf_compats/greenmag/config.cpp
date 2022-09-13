b0df93c3563dfa8e4469c915b34781d666aef352
Nikita S <skippiedipie@gmail.com>
2022-09-13 20:16:59 +0200

    Revert "Restructure, cleanup and fix typo in base"
    This reverts commit 5d6429a9fe32cc2e2b6f4dd558edd13359318ca9.

--- cvo_rhsafrf_compats/greenmag/config.cpp
+++ cvo_rhsafrf_compats/greenmag/config.cpp
@@ -13,7 +13,6 @@ class CfgPatches {
 
 		units[] = {};
 		weapons[] = {
-			// 5.45x39 Ammo
 			"CVO_GreenMag_AFRF_Ammo_545x39_7N6_1Rnd",
 			"CVO_GreenMag_AFRF_Ammo_545x39_7N6_30Rnd",
 			"CVO_GreenMag_AFRF_Ammo_545x39_7N6_60Rnd",
@@ -34,17 +33,7 @@ class CfgPatches {
 			"CVO_GreenMag_AFRF_Ammo_545x39_7T3M_60Rnd",
 			"CVO_GreenMag_AFRF_Ammo_545x39_7U1_1Rnd",
 			"CVO_GreenMag_AFRF_Ammo_545x39_7U1_30Rnd",
-			"CVO_GreenMag_AFRF_Ammo_545x39_7U1_60Rnd",
-			// 7.62x39 Ammo
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231_1Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231_30Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231_60Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231P_1Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231P_30Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231P_60Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231U_1Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231U_30Rnd",
-			"CVO_GreenMag_AFRF_Ammo_762x39_57N231U_60Rnd"
+			"CVO_GreenMag_AFRF_Ammo_545x39_7U1_60Rnd"
 		};
 	};
 };
@@ -54,7 +43,6 @@ class CfgMagazines {
 	class CA_Magazine;
 
 	#include "magazines\afrf_545x39.hpp"
-	#include "magazines\afrf_762x39.hpp"
 
 };
 
@@ -96,7 +84,6 @@ class CfgWeapons {
 
 	// Ammo
 	#include "ammo\afrf_545x39.hpp"
-	#include "ammo\afrf_762x39.hpp"
 
 	// Belts
 	
