f2f142aebfdf626d386082105e8f0d8965b2ecde
SkippieDippie <skippiedipie@gmail.com>
2022-09-14 02:06:08 +0200

    Changed to use macros instead of hardcoded strings

--- cvo_rhsafrf_compats/greenmag/magazines/rhs_afrf_545x39.hpp
+++ cvo_rhsafrf_compats/greenmag/magazines/rhs_afrf_545x39.hpp
@@ -6,25 +6,25 @@ class rhs_30Rnd_545x39_AK : CA_Magazine {
 };
 
 class rhs_30Rnd_545x39_7N6_AK : rhs_30Rnd_545x39_AK {
-	greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_545x39_7N6_1Rnd";
+	greenmag_ammo = QAMMO_CLASS(545x39_7N6_1Rnd);
 };
 
 class rhs_30Rnd_545x39_7N6M_AK : rhs_30Rnd_545x39_7N6_AK {
-	greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_545x39_7N6M_1Rnd";
+	greenmag_ammo = QAMMO_CLASS(545x39_7N6M_1Rnd);
 };
 
 class rhs_30Rnd_545x39_7N10_AK : rhs_30Rnd_545x39_7N6_AK {
-	greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_545x39_7N10_1Rnd";
+	greenmag_ammo = QAMMO_CLASS(545x39_7N10_1Rnd);
 };
 
 class rhs_30Rnd_545x39_7N22_AK : rhs_30Rnd_545x39_7N10_AK {
-	greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_545x39_7N22_1Rnd";
+	greenmag_ammo = QAMMO_CLASS(545x39_7N22_1Rnd);
 };
 
 class rhs_30Rnd_545x39_AK_green : rhs_30Rnd_545x39_7N6_AK {
-	greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_545x39_7T3M_1Rnd";
+	greenmag_ammo = QAMMO_CLASS(545x39_7T3M_1Rnd);
 };
 
 class rhs_30Rnd_545x39_7U1_AK : rhs_30Rnd_545x39_7N10_AK {
-	greenmag_ammo = "CVO_GreenMag_AFRF_Ammo_545x39_7U1_1Rnd";
+	greenmag_ammo = QAMMO_CLASS(545x39_7U1_1Rnd);
 };
